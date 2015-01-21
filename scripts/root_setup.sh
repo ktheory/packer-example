cd1fa084dc4afb034df5874afd7bdc327a0272f4
Aaron Suggs <aaron@ktheory.com>
2015-01-20 23:03:17 -0500

    Use 'ksr' user instead of vagrant

--- scripts/root_setup.sh
+++ scripts/root_setup.sh
@@ -14,7 +14,7 @@ sudo apt-get -y -q install curl wget git tmux firefox xvfb vim
 
 # Setup sudo to allow no-password sudo for "admin"
 groupadd -r admin
-usermod -a -G admin vagrant
+usermod -a -G admin ksr
 cp /etc/sudoers /etc/sudoers.orig
 sed -i -e '/Defaults\s\+env_reset/a Defaults\texempt_group=admin' /etc/sudoers
 sed -i -e 's/%admin ALL=(ALL) ALL/%admin ALL=NOPASSWD:ALL/g' /etc/sudoers
